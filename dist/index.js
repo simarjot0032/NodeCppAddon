5df45f9140e018637e5ea9d12ef9c1c02e292174
simarjot <simarjot0032@gmail.com>
2025-05-20 08:28:40 +0530

    .gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 const addon = require("../build/Release/HelloWorld.node");
-const hello = new addon.Hello(); // Instantiate the C++ class
+const hello = new addon.Hello();
 console.log(hello.Add(1, 55));
 //# sourceMappingURL=index.js.map
